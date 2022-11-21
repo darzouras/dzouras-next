e0dbdcaf5e55f2dc1a502b464d15cdefb3449019
Darlene Zouras <dzouras@gmail.com>
2022-11-21 16:38:46 -0600

    chore: update to client.js, apiVersion

--- client.js
+++ client.js
@@ -4,5 +4,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: 'o1olwwub', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: true // `false` if you want to ensure fresh data
+  useCdn: true, // `false` if you want to ensure fresh data,
+  apiVersion: '2021-10-21'
 })

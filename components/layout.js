fa789e2ac08b518cbf0c4473bb44e5da715bde21
Darlene Zouras <dzouras@gmail.com>
2022-12-09 15:23:26 -0600

    update: desktop layout adjustment

--- components/layout.js
+++ components/layout.js
@@ -2,10 +2,10 @@ import Navigation from './navigation'
 
 const Layout = ({ children, globalData }) => {
   return (
-    <div className="min-h-screen min-w-screen p-8 bg-swamp text-plum">
-      <div className="max-w-xl mx-auto">
+    <div className="min-h-screen min-w-screen p-8 bg-swamp text-plum md:flex md:items-center md:justify-center">
+      <div className="w-11/12 w-max-w-xl mx-auto md:flex md:items-end md:justify-center md:gap-3">
         <Navigation navLinks={globalData.mainNav} />
-        <main className='md:w-4/5 md:mr-0 md:ml-auto max-w-screen-sm'>
+        <main className='max-w-screen-sm md:w-full md:h-96 md:overflow-scroll md:border-2 md:border-violet md:rounded-md md:p-5 md:bg-sand md:flex-1'>
           {children}
         </main>
       </div>
--- components/navigation.js
+++ components/navigation.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 const Navigation = ({ navLinks }) => {
   return (
-    <nav className='border-2 rounded-md border-lake text-lake bg-sand inline-block mb-4'>
-      <ul className='p-2 flex gap-3'>
+    <nav className='border-2 rounded-md border-violet text-violet bg-sand inline-block mb-4 md:mb-0'>
+      <ul className='p-2 flex gap-3 md:flex-col'>
         {navLinks.map((link) => {
           return (
             <li key={link._key}>

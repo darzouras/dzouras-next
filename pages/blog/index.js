fe9cdbbe84304bb21be46e77ccd5a947c2aa4f88
Darlene Zouras <dzouras@gmail.com>
2022-12-12 14:52:32 -0600

    update: another boolean adjustment?

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,25 +2,39 @@ import Head from 'next/head'
 import client from '../../client'
 import BodyContent from '../../components/body-content'
 
+const publishDate = (date) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }
+  return new Date(date).toLocaleDateString('en-US', options)
+}
+
+const Posts = ({ posts }) => {
+  if (posts === undefined || posts.length === 0)
+    return
+  
+  return (
+    <ul>
+      {posts.map((post) => {
+        return (
+          <li key={post._id} className="mb-4">
+            <strong className="text-lake">{ publishDate(post.publishedAt) }</strong><br />
+            <a href={`/blog/${post.slug.current}`}>{post.title}</a>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
 const BlogIndex = ({globalData, page, posts}) => {
   const { body } = page
+
   return (
     <>
       <Head>
         <title>{`${globalData.siteName} | ${page.title}`}</title>
       </Head>
       <BodyContent body={body} />
-      { posts &&
-        <ul>
-          {posts.map((post) => {
-            return (
-              <li key={post._id}>
-                <a href={`/blog/${post.slug.current}`}>{post.title}</a>
-              </li>
-            )
-          })}
-        </ul>
-      }
+      <Posts posts={posts} />
     </>
   )
 }
